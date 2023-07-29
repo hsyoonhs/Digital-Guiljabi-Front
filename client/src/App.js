49a0dae1a5e0db625c20f338f91804ca3608b009
Commeci <cjtkemfldj@naver.com>
2023-07-29 22:02:31 +0900

    Revert "docs : 정보 상세 페이지 관련 route, export"
    This reverts commit f590d967b55c2331245a04700fc2fd79f3a5d15c.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,17 @@
-import "./styles/App.css";
+import './styles/App.css';
 
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import { Detail, Home } from "./pages";
-import { Navbar } from "./components/Navbar";
+import { Home } from './pages';
+import { Navbar } from './components/Navbar';
 
 function App() {
     return (
         <Router>
-            <Navbar />
+            <Navbar/>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/detail" element={<Detail />} />
             </Routes>
         </Router>
     );
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,1 @@
-export { Home } from "./Home";
-export { Detail } from "./Detail";
+export { Home } from './Home';
