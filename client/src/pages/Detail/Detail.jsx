7e7c2a06e6c2f3be8849c6d3b8d0460f4427ca84
Commeci <cjtkemfldj@naver.com>
2023-07-31 17:23:27 +0900

    feat : 좋아요, 북마크 버튼 기능 추가 좋아요와 북마크 버튼을 누르면 숫자가 올라가고, 다시 누르면 내려가도록 기능을 추가했습니다.

--- client/src/pages/Detail/Detail.jsx
+++ client/src/pages/Detail/Detail.jsx
@@ -8,6 +8,40 @@ import { Write } from "./components/Write";
 
 export const Detail = () => {
     const [comments, setComments] = useState([]);
+    const [post, setPost] = useState({
+        id: 1,
+        title: "후잉이가 쓰는 글",
+        nickname: "나는 후잉",
+        date: "2023-07-29",
+        formData: [
+            {
+                subtitle: "날아가는 후잉",
+                photo: "",
+                text: "날아가는 후잉이가 보입니다. 이제 날 수 있는 오리로 성장했어요!",
+            },
+            {
+                subtitle: "도망가는 후잉",
+                photo: "",
+                text: "도망가는 후잉이는 무슨 죄를 지은 걸까요?",
+            },
+        ],
+        sourceData: [
+            {
+                name: "후잉이의 블로그",
+                url: "http://Huing.com",
+            },
+            {
+                name: "후잉이의 게시판",
+                url: "http://HuingPost.com",
+            },
+        ],
+        actionData: {
+            likes: 432,
+            bookmarks: 304,
+            liked: false,
+            bookmarked: false,
+        },
+    });
 
     const handelCommentSubmit = (commentText) => {
         const newComment = {
@@ -19,52 +53,47 @@ export const Detail = () => {
         setComments((prevComments) => [...prevComments, newComment]);
     };
 
-    const topBarData = {
-        title: "후잉이가 쓰는 글",
-        nickname: "나는 후잉",
-        date: "2023-07-29",
+    const handleLike = () => {
+        setPost((prevPost) => ({
+            ...prevPost,
+            actionData: {
+                ...prevPost.actionData,
+                likes: prevPost.actionData.liked
+                    ? prevPost.actionData.likes - 1
+                    : prevPost.actionData.likes + 1,
+                liked: !prevPost.actionData.liked,
+            },
+        }));
     };
 
-    const formData = [
-        {
-            subtitle: "날아가는 후잉",
-            photo: "",
-            text: "날아가는 후잉이가 보입니다. 이제 날 수 있는 오리로 성장했어요!",
-        },
-        {
-            subtitle: "도망가는 후잉",
-            photo: "",
-            text: "도망가는 후잉이는 무슨 죄를 지은 걸까요?",
-        },
-    ];
-
-    const sourceData = [
-        {
-            name: "후잉이의 블로그",
-            url: "http://Huing.com",
-        },
-        {
-            name: "후잉이의 게시판",
-            url: "http://HuingPost.com",
-        },
-    ];
-
-    const actionData = {
-        likes: 432,
-        bookmarks: 304,
+    const handleBookmark = () => {
+        setPost((prevPost) => ({
+            ...prevPost,
+            actionData: {
+                ...prevPost.actionData,
+                bookmarks: prevPost.actionData.bookmarked
+                    ? prevPost.actionData.bookmarks - 1
+                    : prevPost.actionData.bookmarks + 1,
+                bookmarked: !prevPost.actionData.bookmarked,
+            },
+        }));
     };
 
     return (
         <div>
-            <TopBar contents={topBarData} />
-            {formData.map((formData, index) => (
+            <TopBar contents={post} />
+            {post.formData.map((formData, index) => (
                 <Form key={index} contents={formData} />
             ))}
             <h3>출처</h3>
-            {sourceData.map((sourceData, index) => (
+            {post.sourceData.map((sourceData, index) => (
                 <Source key={index} contents={sourceData} />
             ))}
-            <Action contents={actionData} />
+            <Action
+                contents={post.actionData}
+                handleLike={handleLike}
+                handleBookmark={handleBookmark}
+            />
             <Write submitComments={handelCommentSubmit} />
 
             {comments.map((comment, index) => (
