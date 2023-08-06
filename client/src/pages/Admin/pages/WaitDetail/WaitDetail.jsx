3b795dc3ce7ebda8a454eed819f90c9074b56dcb
bini59 <myuni0234@gmail.com>
2023-08-06 19:33:40 +0900

    feat : 게시물 승인/거부 기능 완료
    - 게시물 승인 -> Detail
    - 게시물 거부 -> Dialog
    - 게시물 상세 정보 들고오는 기능 완료

--- client/src/pages/Admin/pages/WaitDetail/WaitDetail.jsx
+++ client/src/pages/Admin/pages/WaitDetail/WaitDetail.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
 import { WaitDialog } from "./Components/WaitDialog";
 
 import "./style.css"
@@ -6,22 +9,45 @@ import "./style.css"
 
 export const WaitDetail = () => {
 
-    const tempData = {
-        title : "카카오택시 50% 할인 이벤트",
-        author: "루꼴라 피자",
-        category: "애플리케이션",
-        hashTag: ["#카카오택시", "#할인", "#이벤트"],
-        img: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-        introduction: `카카오택시에서 50% 할인 이벤트를 진행합니다. 2021년 8월 31일까지 진행되며, 카카오택시 앱에서 할인쿠폰을 받으세요.`,
-        content: `카카오택시에서 50% 할인 이벤트를 진행합니다. 2021년 8월 31일까지 진행되며, 카카오택시 앱에서 할인쿠폰을 받으세요.`,
-        source: ["https://www.kakaocorp.com/service/KakaoTaxi"]
+    const id = useParams().id;
+    const [data, setData] = useState(null);
 
-    }
+    useEffect(() => {
+        const api = process.env.REACT_APP_API_URL;
+        axios.get(`${api}/api/v1/boards/${id}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                setData(res.data);
+
+            })
+            .catch(err => console.log(err))
+    }, [id])
 
 
     const [dialog, setDialog] = useState(false);
     const openDialog = () => setDialog(true);
     const closeDialog = () => setDialog(false);
+
+    const approve = () => {
+        const api = process.env.REACT_APP_API_URL;
+        axios.post(`${api}/api/v1/admin/boards/${id}/approve`, {}, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`
+            },
+            body: {
+                categoryPkList: [2]
+            }
+        })
+            .then(res => {
+                alert("승인되었습니다.");
+                window.location.href = "/admin/service/wait";
+            })
+            .catch(err => console.log(err))
+    }
 
     return (
         <section className="container">
@@ -29,47 +55,43 @@ export const WaitDetail = () => {
             <div className="wait-header detail-header">
                 <div className="wait-title">
                     <div>제목</div>
-                    <div>{tempData.title}</div>
+                    <div>{data?.title}</div>
                 </div>
                 <div className="wait-author">
                     <div>작성자</div>
-                    <div>{tempData.author}</div>
+                    <div>{data?.writerName}</div>
                 </div>
                 <div className="wait-category">
                     <div>카테고리</div>
-                    <div>{tempData.category}</div>
+                    <div>{data?.categories.map((category, index) => <span key={index}>{category}</span>)}</div>
                 </div>
                 <div className="wait-hashtag">
                     <div>해시태그</div>
-                    <div>{tempData.hashTag.map((tag, index) => <span key={index}>{tag}</span>)}</div>
+                    <div>{data?.tags.map((tag, index) => <span key={index}>{tag}</span>)}</div>
                 </div>
                 <div className="wait-img">
                     <div>대표사진</div>
-                    <div><img src={tempData.img} alt="대표사진"/></div>
+                    <div><img src={data?.thumnail} alt="대표사진"/></div>
                 </div>
             </div>
 
             <div className="wait-body detail-header">
                 <div className="wait-introduction">
                     <div>소개글</div>
-                    <div dangerouslySetInnerHTML={{ __html: tempData.introduction }} />
-                </div>
-                <div className="wait-content">
-                    <div>내용</div>
-                    <div dangerouslySetInnerHTML={{ __html: tempData.content }} />
+                    <div dangerouslySetInnerHTML={{ __html: data?.introduction }} />
                 </div>
                 <div className="wait-source">
                     <div>출처</div>
-                    <div>{tempData.source.map((source, index) => <span key={index}>{source}</span>)}</div>
+                    <div>{data?.sources.map((source, index) => <span key={index}>{source}</span>)}</div>
                 </div>
             </div>
 
             <div className="wait-footer detail-footer">
-                <button>승인하기</button>
+                <button onClick={approve}>승인하기</button>
                 <button onClick={openDialog}>거부하기</button>
             </div>
 
-            {dialog && <WaitDialog close={closeDialog} />}
+            {dialog && <WaitDialog close={closeDialog} id={id} />}
         </section>
     )
 };
