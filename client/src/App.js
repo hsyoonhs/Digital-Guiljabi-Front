c9d7b339e5523d44cf4a822f9857dcfd62b6ee20
Commeci <cjtkemfldj@naver.com>
2023-08-11 20:09:08 +0900

    docs : login관련 코드 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import "./styles/App.css";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
 
 import { Detail, Home, My, SearchInfo } from "./pages";
 import { Navbar } from "./components/Navbar";
 
 function App() {
+    useEffect(() => {
+        const api = process.env.REACT_APP_API_URL;
+        console.log(api);
+        axios
+            .post(`${api}/api/login`, {
+                uid: "commeci",
+            })
+            .then((res) => {
+                if (res.data.token)
+                    localStorage.setItem("token", res.data.token);
+            })
+            .catch((err) => console.log("로그인 오류", err));
+    }, []);
     return (
         <Router>
             <Navbar />
