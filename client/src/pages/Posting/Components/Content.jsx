9831e0f0d73764db70934cda2925b6586e54a17d
bini59 <myuni0234@gmail.com>
2023-08-13 17:11:11 +0900

    feat : posting기능 구현 완료
    - posting 페이지 구현 완료
    - 이미지는 Naver 이미지 서버 구축후 구현
    - API서버와 연결, 데이터 업로드 확인 완료
    - 단계 늘리기 구현완료

--- client/src/pages/Posting/Components/Content.jsx
+++ client/src/pages/Posting/Components/Content.jsx
@@ -1,21 +1,31 @@
-export const Content = ({ no }) => {
+export const Content = ({ props }) => {
+
+    const { no, data, updateData, nextStep, prevStep, addStep } = props;
+
+    const update = (e) => {
+        updateData({
+            ...data,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <div>
             <div className="content-header">
                 <h1>{no}</h1>
-                <input placeholder="제목을 입력해주세요." />
-                <input type="file" />
+                <input name="title" placeholder="제목을 입력해주세요." value={data.title} onChange={update}  />
+                <input name="img_url" type="file"  value={data.img_url} onChange={update} />
             </div>
 
             <div className="content-body">
-                <textarea />
+                <textarea name="content" value={data.content} onChange={update} />
             </div>
 
             <div className="content-footer">
-                <button>뒤로</button>
-                <button>다음</button>
-                <button>단계 추가</button>
-                <button>다음 단계로</button>
+                <button onClick={prevStep}>뒤로</button>
+                {/* <button onClick={nextStep}>다음</button> */}
+                <button onClick={addStep}>단계 추가</button>
+                <button onClick={nextStep}>다음 단계로</button>
             </div>
 
         </div>
--- client/src/pages/Posting/Components/Footer.jsx
+++ client/src/pages/Posting/Components/Footer.jsx
@@ -1,18 +1,27 @@
-export const Footer = () => {
+export const Footer = ({ props, save }) => {
+
+    const { data, prevStep, updateData } = props;
+    const update = (e) => {
+        updateData({
+            ...data,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
         <div className="footer">
             <div>
                 <h1>해시태그 입력</h1>
-                <input type="#해시태그" />
+                <input type="#해시태그" name="tags" onChange={update} value={data.tags}/>
             </div>
             <div>
                 <h1>출처입력</h1>
-                <textarea />
+                <textarea name="source" onChange={update} value={data.source}/>
             </div>
 
             <div className="">
-                <button>뒤로</button>
-                <button>다음 단계로</button>
+                <button onClick={prevStep}>뒤로</button>
+                <button onClick={save}>완료</button>
             </div>
         </div>
     )
