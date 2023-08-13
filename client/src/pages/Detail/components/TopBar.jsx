f129372eb4c1177e1a88cf4395cd74bfcdcaa1cb
Commeci <cjtkemfldj@naver.com>
2023-08-13 18:02:46 +0900

    feat : 정보상세페이지 기능 대부분 연결 삭제 기능은 아직 미완성, 좋아요 북마크 기능도 한 유저가 다시 들어오면 다시 좋아요를 누를 수 있는 오류가 있음

--- client/src/pages/Detail/components/TopBar.jsx
+++ client/src/pages/Detail/components/TopBar.jsx
@@ -5,7 +5,7 @@ export const TopBar = ({ contents, modifyPost, deletePost }) => {
         <div>
             <img src={contents.thumbnailUrl} alt="" />
             <h1>{contents.title}</h1>
-            <img src="" alt="" />
+            <img src="" alt="유저 프로필" />
             <label>{contents.writerName}</label>
             <label>{contents.updateAt}</label>
             <button onClick={modifyPost}>수정</button>
