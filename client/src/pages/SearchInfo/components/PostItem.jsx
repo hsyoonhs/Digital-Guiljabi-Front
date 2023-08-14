0e3d8807793a3009b8cce424ef31ce9c4c284fd1
Commeci <cjtkemfldj@naver.com>
2023-08-14 15:58:40 +0900

    docs : 검색 페이지 더보기 기능 수정 및 카테고리 수정 및 정보상세페이지로 링크 연결

--- client/src/pages/SearchInfo/components/PostItem.jsx
+++ client/src/pages/SearchInfo/components/PostItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const PostItem = ({ post }) => {
     return (
         <div>
             <img alt="" />
-            <h3>{post.title}</h3>
+            <h3>
+                <Link to={`/detailInfo/${post.boardPk}`}>{post.title}</Link>
+            </h3>
             <p>마지막 수정일: {post.updateAt}</p>
             <p>{post.introduction}</p>
             <p>Likes: {post.likeCnt}</p>
