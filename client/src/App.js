33fe8847240a829a7d99218c79d21c866d470a68
bini59 <myuni0234@gmail.com>
2023-08-15 11:34:57 +0900

    feat : 알림 기능 추가
    - Firebase 이용하여 알림 기능 추가

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import { UserLayout } from "./components/Layout/UserLayout";
 import { AdminLayout } from "./components/Layout/AdminLayout";
 import axios from "axios";
 
+import "./Firebase"
+
 function App() {
 
     useEffect(() => {
