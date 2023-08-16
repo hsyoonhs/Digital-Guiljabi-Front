9f6d4bfb5c77d741f05613029867a36c42a2f200
bini59 <myuni0234@gmail.com>
2023-08-17 02:43:39 +0900

    feat: 로그인 페이지 처리
    - 로그인 상태일 시, 마이페이지로 이동

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -5,6 +5,27 @@ import axios from "axios"
 
 
 export const Login = () => {
+    
+    // 로그인 상태를 체크 후, 로그인 상태일 시 /my로 이동
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const api_url = process.env.REACT_APP_API_URL;
+        axios.get(`${api_url}/api/v1/token/validate`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then((response) => {
+                console.log("response: ", response);
+                if(response.data.isTokenValid) {
+                    navigate("/my");
+                }
+            })
+            .catch((error) => {
+                console.error("Error 발생 : ", error);
+            });
+    }, [])
 
 
     const getLogin = async () => {
